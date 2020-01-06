78af31a68b554f04b917e2c493ae49429faf5d5e
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-06 23:32:55 +0300

    Function that other headlines

--- app/main/views.py
+++ app/main/views.py
@@ -40,3 +40,19 @@ def headlines(per_page):
     topheadlines_news = topheadlines(per_page)
     title = 'Top Headlines'
     return render_template('topheadlines.html',title=title, name='Top Headlines' ,news=topheadlines_news)
+
+@main.route('/everything&<int:per_page>')
+def all_news(per_page):
+    '''
+    Function that returns other articles
+    '''
+    # per_page = 40
+    everything_news = everything(per_page)
+    title = 'All News'
+    
+    search_articles = request.args.get('search_query')
+
+    if search_articles:
+        return redirect(url_for('main.search',topic=search_articles))
+    else:
+        return render_template('topheadlines.html', title=title, name='All News', news=everything_news)
